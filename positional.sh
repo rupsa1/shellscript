86ff4957aea3b12e81058487bd0365bfebd3564e
reliable abc <reliableabc@reliables-MacBook-Air.local>
2022-11-02 11:35:35 +0530

    script to check positional parameters

---
+++ positional.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# The below programe is an example of positional parametre
+# Positional parametre is nothing but the commnand line arguments.
+# while executing this script we can give the number of arguments.
+
+echo "programe name is : $0"
+echo "Number of arguments : $#"
+echo "The arguments are : $*"
+echo "first argument : $1"
